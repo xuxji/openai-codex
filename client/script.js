da98421cda5a7a4c29769f4e532044b105b39d91
jerryxjxu <jerryxjxu@tencent.com>
2023-02-07 23:45:51 +0800

    feat: update server host

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:8080/', {
+  const response = await fetch('https://openai-codex-server.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
